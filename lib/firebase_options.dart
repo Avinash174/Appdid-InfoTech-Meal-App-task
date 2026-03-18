d04fa45f66e7a86fe9beb117507a402e7ca0e49d
Avinash175 <avinashmagar15@gmail.com>
2026-03-18 08:40:50 +0530

    docs: Remove redundant comment from `DefaultFirebaseOptions` class.

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -4,7 +4,6 @@ import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
 import 'package:flutter/foundation.dart'
     show defaultTargetPlatform, TargetPlatform;
 
-/// Default [FirebaseOptions] for use with your Firebase apps.
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
     switch (defaultTargetPlatform) {
